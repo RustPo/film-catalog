cf5909607d7ccc460b7669e7ea74610f06c7544b
Denis Akhmetov <rust.futer@gmail.com>
2025-09-07 21:18:41 +0700

    test: исправлен тест

--- src/app/shared/api/movie/movie-api.service.spec.ts
+++ src/app/shared/api/movie/movie-api.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MovieApiService } from './movie-api.service';
+import { basicTestingProviders } from '@shared/providers/basic-testing-provider';
 
 describe('MovieApiService', () => {
   let service: MovieApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [basicTestingProviders],
+    });
     service = TestBed.inject(MovieApiService);
   });
 
