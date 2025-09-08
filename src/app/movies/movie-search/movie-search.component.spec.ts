4ea81f35f29cf0a447f449882e92d88254c2c899
Denis Akhmetov <rust.futer@gmail.com>
2025-09-08 12:40:30 +0700

    test: исправлены тесты

--- src/app/movies/movie-search/movie-search.component.spec.ts
+++ src/app/movies/movie-search/movie-search.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieSearchComponent } from './movie-search.component';
+import { basicTestingProviders } from '@shared/providers/basic-testing-provider';
 
 describe('MovieSearchComponent', () => {
   let component: MovieSearchComponent;
@@ -9,6 +10,7 @@ describe('MovieSearchComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MovieSearchComponent],
+      providers: [basicTestingProviders],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MovieSearchComponent);
